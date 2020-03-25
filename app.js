0f381a2c08dbba60cc157a29037470a008281dd7
root <root@ip-172-31-18-156.ap-southeast-1.compute.internal>
2020-03-25 10:24:56 +0000

    enable https in server and add morgan

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
-process.env.NODE_ENV !== "production" && require("dotenv").config()
+// process.env.NODE_ENV !== "production" && require("dotenv").config())
 const express = require("express");
 const app = express();
 const cors = require("cors");
 const routes = require("./routes");
+const morgan = require('morgan')
 
+app.use(morgan('tiny'))
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(cors());
--- bin/http.js
+++ bin/http.js
@@ -1,6 +1,33 @@
 const app = require("../app");
-const http = require("http");
-const server = http.createServer(app);
-const port = +process.env.PORT;
+// const http = require("http");
+// const server = http.createServer(app);
+// const port = +process.env.PORT;
 
-server.listen(port, () => console.log("This server run on port :", port));
+// server.listen(port, () => console.log("This server run on port :", port));
+
+const fs = require('fs');
+const http = require('http');
+const https = require('https');
+// Certificate
+const privateKey = fs.readFileSync('/etc/letsencrypt/live/api-passworder.nafies.tech/privkey.pem', 'utf8');
+const certificate = fs.readFileSync('/etc/letsencrypt/live/api-passworder.nafies.tech/cert.pem', 'utf8');
+const ca = fs.readFileSync('/etc/letsencrypt/live/api-passworder.nafies.tech/chain.pem', 'utf8');
+
+const credentials = {
+	key: privateKey,
+	cert: certificate,
+	ca: ca
+};
+
+// Starting both http & https servers
+const httpServer = http.createServer(app);
+const httpsServer = https.createServer(credentials, app);
+
+httpServer.listen(80, () => {
+	console.log('HTTP Server running on port 80');
+});
+
+httpsServer.listen(443, () => {
+	console.log('HTTPS Server running on port 443');
+});
+
